6dd273dc52201e7d2f57cf5e0ac7b88ec5cee962
astynax <astynax@users.noreply.github.com>
2025-11-21 16:42:57 +0000

    emacs: sync the config

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,9 +1,13 @@
 (menu-bar-mode -1)
 (setq inhibit-startup-screen t)
 
+(icomplete-mode 1)
+
 (define-key global-map (kbd "M-o") #'other-window)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
   
 (setq backup-directory-alist
       (list (cons ".*" (format "%sbackup/" user-emacs-directory))))
+
+(auto-save-mode -1)
